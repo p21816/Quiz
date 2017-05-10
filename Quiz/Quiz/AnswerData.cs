dc424ee76c070b875c734947a462c70606a38677
Артём <eviljack78@gmail.com>
2017-05-10 17:23:23 +0300

    class hierarchy

---
+++ Quiz/Quiz/AnswerData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    struct AnswerData
+    {
+        // student, who had given the answer
+        Student student;
+        // time, when the answer was given
+        DateTime time;
+        // whether the answer was correct
+        bool isCorrect;
+        // type of the question
+        string questionType;
+    }
+}
---
+++ Quiz/Quiz/Question.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    abstract class Question
+    {
+        public enum QuestionState
+        {
+            NotAsked,
+            Asked,
+            CorrectlyAnswered,
+            IncorrectlyAnswered
+        }
+        public QuestionState State { get; private set; }
+        public Question()
+        {
+            State = QuestionState.NotAsked;
+        }
+        abstract public void Ask(IQuestionForm form);
+        abstract public void CheckAnswer(IQuestionForm form);
+    }
+}
